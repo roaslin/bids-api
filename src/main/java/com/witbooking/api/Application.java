66f397fc215c9c790c70d0dc7e160ba31eae5487
roaslin <roaslin@minastirith.(none)>
2016-03-04 20:22:00 +0100

    Initial commit

---
+++ src/main/java/com/witbooking/api/Application.java
@@ -0,0 +1,19 @@
+package com.witbooking.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Main entry point for the Application.
+ *
+ */
+@EnableJpaRepositories
+@EnableTransactionManagement
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
